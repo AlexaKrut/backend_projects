f3710ea3fc781c5b8c3b238605c6cb40f406ca69
unknown <alexandra.krutihina@ya.ru>
2024-08-27 12:34:24 +0300

    First commit!

---
+++ task_tracker_CLI/src/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int choice;
+
+    cout << "Task Tracker Menue" << endl;
+    cout << "1. Add task" << endl;
+    cout << "2. Update task" << endl;
+    cout << "3. Delete task" << endl;
+    cout << "4. Mark a task as in progress" << endl;
+    cout << "5. Mark a task as done" << endl;
+    cout << "6. List all tasks" << endl;
+    cout << "7. List all tasks that are done" << endl;
+    cout << "8. List all tasks that are not done" << endl;
+    cout << "9. List all tasks that are in progress" << endl;
+    
+    cin >> choice;
+    switch (choice) {
+        case 1:
+        // Add task
+        break;
+        case 2:
+        // Update task
+        break;
+        case 3:
+        // Delete task
+        break;
+        case 4:
+        // Mark a task as in progress
+        break;
+        case 5:
+        // Mark a task as done
+        break;
+        case 6:
+        // List all tasks
+        break;
+        case 7:
+        // List all tasks that are done
+        break;
+        case 8:
+        // List all tasks that are not done
+        break;
+        case 9:
+        // List all tasks that are in progress
+        break;
+    }
+    return 0;
+}
